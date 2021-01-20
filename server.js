4fe066f0d05b07036635766c97f802ff74335fd5
Daniel Sasser <desasser79@gmail.com>
2021-01-19 16:09:41 -0800

    switch is built for functions

--- server.js
+++ server.js
@@ -19,4 +19,66 @@ const connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId + "\n");
+  startUp();
 });
+
+function startUp() {
+  inquirer.prompt(
+    {
+      type: 'list',
+      choices: ['Add department', 'Add role', 'Add employee','View department', 'View role', 'View employee', 'Update employee roles'],
+      message: 'What would you like to do?',
+      name: 'action'
+  }
+  ).then(answers => {
+    console.log(answers);
+    switch (answers.action) {
+      case 'Add department':
+        addDepartment();
+        break;
+      case 'Add role':
+        addRole();
+        break;
+      case 'Add employee':
+        addEmployee();
+        break;
+      case 'View department':
+        viewDepartment();
+        break;
+      case 'View role':
+        viewRole();
+        break;
+      case 'View employee':
+        viewEmployee();
+        break;
+      case 'Update employee roles':
+        updateRoles();
+        break;
+      default:
+        connection.end();
+        break;
+    }
+  })
+};
+
+function addDepartment() {
+  
+}
+function addRole() {
+
+}
+function addEmployee() {
+
+}
+function viewDepartment() {
+
+}
+function viewRole() {
+
+}
+function viewEmployee() {
+
+}
+function updateRoles() {
+
+}
